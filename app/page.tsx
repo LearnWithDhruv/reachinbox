61fb81154dca14933dcc29a158d49802e6888def
Dhruv Khatter <112469222+LearnWithDhruv@users.noreply.github.com>
2024-08-14 17:51:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import AuthButton from '../components/AuthButton';
 import ThemeToggle from '../components/ThemeToggle';
